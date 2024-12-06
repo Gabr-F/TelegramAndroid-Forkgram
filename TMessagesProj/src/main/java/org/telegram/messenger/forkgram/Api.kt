strannik-j/TelegramAndroid

Added ability to delete all your messages in group.

TMessagesProj/src/main/java/org/telegram/messenger/forkgram/Api.kt
@@ -4,6 +4,7 @@ import org.telegram.messenger.AccountInstance
 import org.telegram.messenger.MessageObject
 import org.telegram.tgnet.TLObject
 import org.telegram.tgnet.TLRPC
+import kotlin.math.min
 
 object ForkApi {
 
@@ -40,5 +41,71 @@ fun TLRPCMessages(
         };
     }
 }
+
+@JvmStatic
+fun FullChannel(
+        currentAccount: Int,
+        channelId: Long,
+        finish: (TLRPC.TL_messages_chatFull) -> Unit) {
+    if (channelId == 0L) {
+        return;
+    }
+    val accountInstance = AccountInstance.getInstance(currentAccount);
+    val api = accountInstance.connectionsManager;
+
+    val req = TLRPC.TL_channels_getFullChannel();
+    req.channel = accountInstance.messagesController.getInputChannel(channelId);
+
+    api.sendRequest(req) { response: TLObject?, msgErr: TLRPC.TL_error? ->
+        if (response != null) {
+            finish(response as TLRPC.TL_messages_chatFull);
+        }
+    };
+}
+
+@JvmStatic
+fun SearchAllMessages(
+        currentAccount: Int,
+        peer: TLRPC.InputPeer,
+        from: TLRPC.InputPeer,
+        finish: (ArrayList<TLRPC.Message>) -> Unit) {
+    val api = AccountInstance.getInstance(currentAccount).connectionsManager;
+
+    val found = ArrayList<TLRPC.Message>();
+    val performPtr = ArrayList<(Int) -> Int>();
+
+    val perform = {offsetId: Int ->
+        val req = TLRPC.TL_messages_search();
+        req.peer = peer;
+        req.from_id = from;
+        req.limit = 100;
+        req.offset_id = offsetId;
+        req.filter = TLRPC.TL_inputMessagesFilterEmpty();
+        req.q = "";
+        req.flags = (0 or 1);
+
+        api.sendRequest(req) { msgResponse: TLObject?, msgErr: TLRPC.TL_error? ->
+            val msgRes = if (msgResponse != null)
+                msgResponse as TLRPC.messages_Messages
+            else TLRPC.TL_messages_messagesNotModified();
+
+            if (msgRes.messages.isEmpty()) {
+                finish(found);
+                return@sendRequest;
+            }
+
+            var newOffsetId = msgRes.messages.get(0).id;
+            for (message in msgRes.messages) {
+                found.add(message);
+                newOffsetId = min(newOffsetId, message.id);
+            }
+            performPtr[0](newOffsetId);
+        };
+    };
+
+    performPtr.add(perform);
+    perform(0);
+}
+
 
 }

Gabr-F/TelegramAndroid-Forkgram

TMessagesProj/src/main/java/org/telegram/messenger/forkgram/Api.kt
@@ -4,6 +4,7 @@ import org.telegram.messenger.AccountInstance
 import org.telegram.messenger.MessageObject
 import org.telegram.tgnet.TLObject
 import org.telegram.tgnet.TLRPC
+import kotlin.math.min
 
 object ForkApi {
 
@@ -40,5 +41,74 @@ fun TLRPCMessages(
         };
     }
 }
+
+@JvmStatic
+fun FullChannel(
+        currentAccount: Int,
+        channelId: Long,
+        finish: (TLRPC.TL_messages_chatFull) -> Unit) {
+    if (channelId == 0L) {
+        return;
+    }
+    val accountInstance = AccountInstance.getInstance(currentAccount);
+    val api = accountInstance.connectionsManager;
+
+    val req = TLRPC.TL_channels_getFullChannel();
+    req.channel = accountInstance.messagesController.getInputChannel(channelId);
+
+    api.sendRequest(req) { response: TLObject?, msgErr: TLRPC.TL_error? ->
+        if (response != null) {
+            finish(response as TLRPC.TL_messages_chatFull);
+        }
+    };
+}
+
+@JvmStatic
+fun SearchAllMessages(
+        currentAccount: Int,
+        peer: TLRPC.InputPeer,
+        from: TLRPC.InputPeer,
+        step: (ArrayList<TLRPC.Message>) -> Unit,
+        finish: () -> Unit) {
+    val api = AccountInstance.getInstance(currentAccount).connectionsManager;
+
+    val found = ArrayList<TLRPC.Message>();
+    val performPtr = ArrayList<(Int) -> Int>();
+
+    val perform = {offsetId: Int ->
+        val req = TLRPC.TL_messages_search();
+        req.peer = peer;
+        req.from_id = from;
+        req.limit = 100;
+        req.offset_id = offsetId;
+        req.filter = TLRPC.TL_inputMessagesFilterEmpty();
+        req.q = "";
+        req.flags = (0 or 1);
+
+        api.sendRequest(req) { msgResponse: TLObject?, msgErr: TLRPC.TL_error? ->
+            val msgRes = if (msgResponse != null)
+                msgResponse as TLRPC.messages_Messages
+            else TLRPC.TL_messages_messagesNotModified();
+
+            if (msgRes.messages.isEmpty()) {
+                finish();
+                return@sendRequest;
+            }
+
+            var newOffsetId = msgRes.messages.get(0).id;
+            for (message in msgRes.messages) {
+                found.add(message);
+                newOffsetId = min(newOffsetId, message.id);
+            }
+            step(found);
+            found.clear();
+            performPtr[0](newOffsetId);
+        };
+    };
+
+    performPtr.add(perform);
+    perform(0);
+}
+
 
 }